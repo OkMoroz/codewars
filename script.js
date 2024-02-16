3f3742a18f33345ab3bbc1dd4ce03f11db688918
OkMoroz <k.moroz.mail@gmail.com>
2024-02-16 06:33:50 +0200

    task4

--- script.js
+++ script.js
@@ -155,3 +155,34 @@ function answer3() {
 // const answer1 = () => "no"
 // const answer2 = () => "no"
 // const answer3 = () => "yes"
+
+// Task #4
+// I've written five functions. Each function receives a parameter arr which is an array. Complete the functions using arr inside the function bodies.
+
+//     1. getLength(arr)    should return length of arr
+//     2. getFirst(arr)     should return the first element of arr
+//     3. getLast(arr)      should return the last element of arr
+//     4. pushElement(arr)  should push an element to arr, and then return arr
+//     5. popElement(arr)   should pop an element from arr, and then return arr
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+function getLength(arr) {
+  //return length of arr
+  return arr.length;
+}
+function getFirst(arr) {
+  //return the first element of arr
+  return arr[0];
+}
+function getLast(arr) {
+  //return the last element of arr
+  return arr[arr.length - 1];
+}
+function pushElement(arr) {
+  let el = 1;
+  arr.push(el);
+  return arr;
+}
+function popElement(arr) {
+  arr.pop();
+  return arr;
+}

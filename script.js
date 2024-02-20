ad81cb5241adf12c1a98a4c540aeaf819e3543bd
OkMoroz <k.moroz.mail@gmail.com>
2024-02-20 05:59:57 +0200

    Task 9

--- script.js
+++ script.js
@@ -280,3 +280,35 @@ function howManydays(month) {
       return undefined;
   }
 }
+// Task #9
+// Complete function padIt, which accepts 2 parameters:
+
+// str: a string representing the string to pad. We will pad it with "*" symbols on its left side and on its right side.
+// n: a number indicating how many times to pad the string.
+// Behaviour
+// You need to write a loop statement within the function that loops n times. Each time through the loop it will add one * to str, alternating on which side it is padded: the first time will add a * to the left side of str, the second time will add a * to the right side, and so on.
+
+// Finally, return the padded string.
+
+function padIt(str, n) {
+  let paddedStr = str;
+  let i = 0;
+
+  do {
+    if (i % 2 === 0) {
+      paddedStr = '*' + paddedStr;
+    } else {
+      paddedStr = paddedStr + '*';
+    }
+    i++;
+  } while (i < n);
+
+  return paddedStr;
+}
+////
+function padIt(str, n) {
+  while (n > 0) {
+    str = n-- % 2 ? '*' + str : str + '*';
+  }
+  return str;
+}
